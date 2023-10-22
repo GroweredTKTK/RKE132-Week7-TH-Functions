457f1ea371f753dc71462debc4866c45eb6b8a4a
GroweredTKTK <thomas.saija@tktk.ee>
2023-10-22 22:46:00 +0300

    add week7 code

---
+++ DiscountCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿//total < 10 - 1%
+//total >= 10 && < 20 - 5%
+//total >= 20 - 10%
+
+Console.WriteLine("What is your total?");
+int userInput = int.Parse(Console.ReadLine());//int.Parse acts same as the Int32.Parse
+
+double discount = CalculateDicount(userInput);
+
+Console.WriteLine($"Your discount is: {discount}%");
+
+double newTotal = CalculateNewTotal(userInput, discount);
+
+Console.WriteLine($"Your updated total with {discount}%: {newTotal}");
+
+
+static double CalculateDicount(int total)
+{
+    if (total > 10)
+    {
+        return 1;
+    }
+    else if (total >= 1 && total < 20)
+    {
+        return 5;
+    }
+    else
+    {
+        return 10;
+    }
+}
+
+static double CalculateNewTotal(double total, double discount)
+{
+    double result = total - (total * discount) / 100;
+    return result;
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out)");
+string userChoise = Console.ReadLine();
+
+if (userChoise == "in") 
+{
+    PrintHello();
+}
+else if (userChoise == "out")
+{
+    PrintGoodBye();
+}
+else
+{
+    WrongAnswer();
+}
+
+
+static void WrongAnswer()
+{
+    Console.WriteLine("Wrong answer");
+}
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later");
+}
